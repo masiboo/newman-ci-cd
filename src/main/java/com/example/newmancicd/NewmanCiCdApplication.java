3289f39079aafbc81d2f88c2c5f50c30e334c615
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2022-11-10 08:53:25 +0100

    Add genarated project

---
+++ src/main/java/com/example/newmancicd/NewmanCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.newmancicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewmanCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewmanCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newmancicd/NewmanCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newmancicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewmanCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
